c5374044b0a1e5d94dd43301d9d8edb356e3ebf5
wperera6 <wperera6@gmail.com>
2015-07-29 14:56:52 -0400

    drop box set up for Betsy finished

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,7 +1,12 @@
 class Listing < ActiveRecord::Base
-  has_attached_file :image, :styles => { :medium => "200x>", :thumb => "100x100>" }, :default_url => "noimg.png",
-                    :storage => :dropbox,
-                    :dropbox_credentials => Rails.root.join("config/dropbox.yml")
-                    
-  validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
+  if Rails.env.development?
+    has_attached_file :image, :styles => { :medium => "200x>", :thumb => "100x100>" }, :default_url => "noimg.png"
+     validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
+  else
+    has_attached_file :image, :styles => { :medium => "200x>", :thumb => "100x100>" }, :default_url => "noimg.png",
+                      :storage => :dropbox,
+                      :dropbox_credentials => Rails.root.join("config/dropbox.yml"),
+                      :path => ":style/:id_:filename"
+    validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
+  end
 end
