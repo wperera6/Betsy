ba9b17691aa615d0365766bc6139c754cdf5876a
wperera6 <wperera6@gmail.com>
2015-07-30 13:16:41 -0400

    added name field to users in betsy

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150730135803) do
+ActiveRecord::Schema.define(version: 20150730170417) do
 
   create_table "listings", force: :cascade do |t|
     t.string   "name"
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 20150730135803) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
