2f7c6c3f5f98760a7629119c4e6e3b970dd07f34
wperera6 <wperera6@gmail.com>
2015-08-05 13:58:29 -0400

    added order history pages to Betsy

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,15 @@ Rails.application.routes.draw do
   
   devise_for :users
   resources :listings do
-    resources :orders
+    #prevent users from editing order history, only new and create are enabled
+    resources :orders, only: [:new, :create]
   end
   
   get 'pages/about'
   get 'pages/contact'
   get 'seller' => "listings#seller"
+  get 'sales' => "orders#sales"
+  get 'purchases' => "orders#purchases"
 
   root 'listings#index'
   
